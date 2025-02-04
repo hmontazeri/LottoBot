83428d67cc977ce8a493e899ceeaacd2abe6d7fc
Hamed Montazeri <hamed.mon@gmail.com>
2025-02-04 13:47:08 +0000

    fixes

--- database/check_data.ts
+++
@@ -1,9 +0,0 @@
-import User from '#models/user'
-
-async function checkDatabase() {
-  const users = await User.all()
-  console.log('Users in database:', users)
-  process.exit(0)
-}
-
-checkDatabase()
