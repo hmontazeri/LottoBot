157b46c810db0a753113f0441f233814ede3daa1
Hamed Montazeri <hamed.mon@gmail.com>
2025-02-06 08:17:21 +0000

    kernel updated

--- start/kernel.ts
+++ start/kernel.ts
@@ -23,14 +23,22 @@ const logger = new Logger({ enabled: true, name: 'cron' })
 cron.schedule('30 19 * * 3,6', async () => {
   // 3 = Mittwoch, 6 = Samstag
   logger.info(`Running Lotto 6aus49 task ${new Date().toLocaleString()})`)
-  await new TelegramsController().sendLottoResults()
+  try {
+    await new TelegramsController().sendLottoResults()
+  } catch (error) {
+    logger.error('Error sending lotto results:', error)
+  }
 })
 
 // Eurojackpot nur am Dienstag & Freitag um 20:30 Uhr senden
 cron.schedule('30 20 * * 2,5', async () => {
   // 2 = Dienstag, 5 = Freitag
   logger.info(`Running Eurojackpot task ${new Date().toLocaleString()})`)
-  await new TelegramsController().sendLottoResults()
+  try {
+    await new TelegramsController().sendLottoResults()
+  } catch (error) {
+    logger.error('Error sending lotto results:', error)
+  }
 })
 
 /**
